62e1e7186524cf4f24c3455f4497e1d210de2cc3
liang <LZD95051@sjtu.edu.cn>
2018-07-06 10:41:45 +0800

    add some license in file

--- src/rrt_drawing/rrt_drawing.h
+++ src/rrt_drawing/rrt_drawing.h
@@ -1,3 +1,22 @@
+//Copyright 2015 Georgia Tech RoboJackets
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//		http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//		limitations under the License.
+
+// This file is used for defining some parameters and run RRT algorithm. It is similar with RRTWidget in
+// the original version(https://github.com/RoboJackets/rrt/blob/master/src/rrt-viewer/RRTWidget.cpp)
+// But we do not use QT but simply use opencv to show the result.
+
+
 #pragma once
 
 #include <fstream>
